6afb3ab41cbc7420a595fefc944c19513532e154
SereneLuta <36087103+SereneLuta@users.noreply.github.com>
2021-04-07 08:54:18 -0400

    Initial commit

---
+++ TimeClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using CacheCow.Client;
+using CacheCow.Client.RedisCacheStore;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TimeClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = ClientExtensions.CreateClient(new RedisStore("localhost:6379"));
+            client.BaseAddress = new Uri("http://localhost:1337");
+
+            while (true)
+            {
+                var response = await client.GetAsync("/time");
+
+                Console.WriteLine(response.Headers.CacheControl.ToString());
+
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+
+                Console.WriteLine("Hit enter to call it again or type 'done' to quit");
+                var q = Console.ReadLine();
+                if (q == "done")
+                {
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
